96aeaf20d250317a8ff24faeedb59ca87463ab10
danish0511 <dsdd0511@gmail.com>
2024-07-26 23:30:36 +0530

    live features

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,40 +1,41 @@
-"use client";
+'use client';
 
-import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
-import React, { useEffect, useRef, useState } from "react";
-import Header from "./Header";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { Editor } from "./editor/Editor";
-import ActiveCollaborators from "./ActiveCollaborators";
-import Loader from "./loader";
-import { Input } from "./ui/input";
-import Image from "next/image";
-import { updateDocument } from "@/lib/actions/room.actions";
+import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import ActiveCollaborators from './ActiveCollaborators';
+import { useEffect, useRef, useState } from 'react';
+import { Input } from './ui/input';
+import Image from 'next/image';
+import { updateDocument } from '@/lib/actions/room.actions';
+import Loader from './loader';
+// import ShareModal from './ShareModal';
 
-const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
-
-  const currentUserType = 'editor'
+const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
 
   const containerRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const updateTitleHandler = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      setLoading(true)
-      try {
-        if (documentTitle !== roomMetadata.title) {
-          const updatedDocument = await updateDocument(roomId, documentTitle);
+    if(e.key === 'Enter') {
+      setLoading(true);
 
-          if (!updatedDocument) {
-            setEditing(false)
+      try {
+        if(documentTitle !== roomMetadata.title) {
+          const updatedDocument = await updateDocument(roomId, documentTitle);
+          
+          if(updatedDocument) {
+            setEditing(false);
           }
         }
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
+
       setLoading(false);
     }
   }
@@ -59,20 +60,20 @@ const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
       inputRef.current.focus();
     }
   }, [editing])
-
+  
 
   return (
     <RoomProvider id={roomId}>
-      <ClientSideSuspense fallback={<Loader/>}>
+      <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
             <div ref={containerRef} className="flex w-fit items-center justify-center gap-2">
               {editing && !loading ? (
-                <Input
+                <Input 
                   type="text"
                   value={documentTitle}
                   ref={inputRef}
-                  placeholder="Enter Title"
+                  placeholder="Enter title"
                   onChange={(e) => setDocumentTitle(e.target.value)}
                   onKeyDown={updateTitleHandler}
                   disable={!editing}
@@ -84,27 +85,33 @@ const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
                 </>
               )}
 
-              {currentUserType === 'editor' && !editing &&(
-                <Image
-                  src='/assets/icons/edit.svg'
-                  alt='edit'
+              {currentUserType === 'editor' && !editing && (
+                <Image 
+                  src="/assets/icons/edit.svg"
+                  alt="edit"
                   width={24}
                   height={24}
                   onClick={() => setEditing(true)}
-                  className='pointer'
+                  className="pointer"
                 />
               )}
 
-              {currentUserType !== 'editor' && !editing &&(
-                <p className="view-only">View Only</p>
+              {currentUserType !== 'editor' && !editing && (
+                <p className="view-only-tag">View only</p>
               )}
 
               {loading && <p className="text-sm text-gray-400">saving...</p>}
-
-
             </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveCollaborators />
+
+              {/* <ShareModal 
+                roomId={roomId}
+                collaborators={users}
+                creatorId={roomMetadata.creatorId}
+                currentUserType={currentUserType}
+              /> */}
+
               <SignedOut>
                 <SignInButton />
               </SignedOut>
@@ -113,11 +120,11 @@ const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
               </SignedIn>
             </div>
           </Header>
-          <Editor />
+        <Editor roomId={roomId} currentUserType={currentUserType} />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
-  );
-};
+  )
+}
 
-export default CollaborativeRoom;
+export default CollaborativeRoom
